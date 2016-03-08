503cfc55e957308eb6a909cf2daeaae571e952f3
stayhigh <redhero0702@gmail.com>
2016-03-08 18:26:16 +0800

    checking the number of the parameters

--- http_methods_checker.sh
+++ http_methods_checker.sh
@@ -1,6 +1,16 @@
 http_server_host=$1
 http_server_port=$2
 
+function usage(){
+    echo "$0 <http_server_host> <http_server_port>"
+}
+
+if test $# -ne 2
+then 
+    usage
+    exit
+fi
+
 for method in OPTIONS GET HEAD POST PUT DELETE TRACE CONNECT ; do 
     echo "\n\nTrying $method\n\n" 
     echo "$method / HTTP/1.1\nHost: $http_server_host\nConnection: close\n\n" | nc $http_server_host $http_server_port | head 
